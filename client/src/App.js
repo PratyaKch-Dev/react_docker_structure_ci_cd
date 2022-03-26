27b930eacdf8801dcafc2ebf1fe43aef5be03581
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2022-03-26 20:25:38 +0700

    fix deploy

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Chage bucket update EB new Chage id
+          Learn React Chage bucket update EB new Chage id test
         </a>
       </header>
     </div>
