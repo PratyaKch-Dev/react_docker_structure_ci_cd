382fb57358f1deb798519accee274c5ed2823790
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2022-03-26 16:59:28 +0700

    deploy EB

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Chage bucket
+          Learn React Chage bucket EB
         </a>
       </header>
     </div>
