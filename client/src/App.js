033188e5d3c8c8578723ca95a8e33a7972671b4c
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2022-03-26 16:08:45 +0700

    change bucket

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React JS JS
+          Learn React Chage bucket
         </a>
       </header>
     </div>
