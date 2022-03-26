7591019ae885f0b65bfc18e2bacc92d93a981907
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2022-03-26 17:31:32 +0700

    new update

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Chage bucket update EB
+          Learn React Chage bucket update EB new
         </a>
       </header>
     </div>
